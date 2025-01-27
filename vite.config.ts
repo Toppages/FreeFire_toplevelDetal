bdf65b2be4cbe45db700ff5988f91e45df8023f8
evanansemprun <evanansemprun@gmail.com>
2025-01-27 16:47:30 -0400

    log

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://Toppages.github.io/Top-level-redeem ",
+  base:"https://Toppages.github.io/Top-level-redeem",
 })
